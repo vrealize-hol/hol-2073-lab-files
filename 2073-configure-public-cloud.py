b25f51dbba3611ccfc99b6d0fabf01ae4462d9dc
parsog <55463002+parsog@users.noreply.github.com>
2020-03-31 16:01:19 -0700

    Update 2073-configure-public-cloud.py

--- 2073-configure-public-cloud.py
+++ 2073-configure-public-cloud.py
@@ -847,7 +847,7 @@ if hol:
     if assigned_pod[0] == 'T0':
         # checking to see if any pod credentials are available
         print('\n\n\nWARNING - No Hands On Labs public cloud credentials are available now!!')
-        print('There are a limited set of credentials available to share across all active labs')
+        print('There are a limited set of credentials available to share across all active labs and they are all in use')
         print('Please end this lab and try again later')
         payload = { "text": f"*WARNING - There are no credential sets available for {lab_user}*" }
         send_slack_notification(payload)
