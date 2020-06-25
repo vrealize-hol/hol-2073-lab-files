029f0a461f33a7ea9c1f5237155e4211ce143739
parsog <55463002+parsog@users.noreply.github.com>
2020-06-25 08:50:06 -0700

    get oldest creds even if none available

--- 2073-configure-public-cloud.py
+++ 2073-configure-public-cloud.py
@@ -79,26 +79,34 @@ def get_available_pod():
         ProjectionExpression="pod, in_use"
     )
     pods = response['Items']
-
     # the number of pods not reserved
     num_not_reserved = len(pods)
-
-    available_pods = 0  #set counter to zero
+    available_pods = 0  # set counter to zero
     pod_array = []
     for i in pods:
-        if i['in_use'] == 0:    #pod is available
-            available_pods += 1 #increment counter
+        if i['in_use'] == 0:  # pod is available
+            available_pods += 1  # increment counter
             pod_array.append(i['pod'])
 
-    if available_pods == 0:     #no pod credentials are available
-        return("T0", num_not_reserved, available_pods)
-            
-    #get random pod from those available
-    dt = datetime.datetime.microsecond
-    seed(dt)
-    rand_int = randint(0,available_pods-1)
-    pod = pod_array[rand_int]
-
+    if available_pods == 0:  # all credentials are assigned
+        # get the oldest credentials and re-use those
+        response = table.scan(
+            FilterExpression=Attr('check_out_epoch').gt(0),
+            ProjectionExpression="pod, check_out_epoch"
+        )
+        pods = response['Items']
+        oldest = pods[0]['check_out_epoch']
+        pod = pods[0]['pod']
+        for i in pods:
+            if i['check_out_epoch'] < oldest:
+                pod = i['pod']
+                oldest = i['check_out_epoch']
+    else:
+        # get random pod from those available
+        dt = datetime.datetime.microsecond
+        seed(dt)
+        rand_int = randint(0, available_pods-1)
+        pod = pod_array[rand_int]
     return(pod, num_not_reserved, available_pods)
 
 def get_creds(cred_set,vlp_urn_id):
